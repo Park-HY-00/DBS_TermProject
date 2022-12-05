576aa98e4ef54c98a71b8a305c1a6eb461c3adee
Park-HY-00 <phy000115@naver.com>
2022-12-05 19:50:19 +0900

    Update the function of menu 6

--- sleep.py
+++ sleep.py
@@ -1,4 +1,5 @@
 import pymysql
+import datetime as dt
 
 db = pymysql.connect(host='192.168.155.101', user='hypark',
                      password='parkhy0115!', port=3308, db='sleep', charset='utf8')
@@ -6,7 +7,7 @@ db = pymysql.connect(host='192.168.155.101', user='hypark',
 cursor = db.cursor()
 
 while 1:
-    print("-------------------------------------------")
+    print("\n\n-------------------------------------------")
     print("[2020069022]------------[Hyeong-Yeong Park]")
     print("[-------Sleep Time & Condition Diary--------]")
     print("[-----------View all sleep data-------------]")
@@ -23,13 +24,20 @@ while 1:
         sql = "INSERT INTO USER (Fname, Lname, Name, ID, Password, Bdate, Sex) VALUES (%s, %s, %s, %s, %s, %s, %s)"
         print(
             "Input your personal information (Fname, Lname, Name, ID, Passwrod, Bdate, Sex)")
+        print("What is your First name?")
         Fname = str(input())
+        print("What is your Last name?")
         Lname = str(input())
-        Name = str(input())
+        Name = Fname + " " + Lname
+        print("Your ID: ")
         ID = str(input())
+        print("Password: ")
         Password = str(input())
+        print("What is your Birth-day?")
         Bdate = str(input())    # DATE datatype으로 받아오는 수정 필요
+        print("What is your sex? (Male/Female)")
         Sex = str(input())
+        print("Name: ", Name, "\nID: ", ID, "\nB-Day: ", Bdate, "\nSex: ", Sex)
         cursor.execute(sql, (Fname, Lname, Name, ID, Password, Bdate, Sex))
     elif menu == 2:
         print("[-------------Sleep Time Record-------------]")
@@ -55,15 +63,32 @@ while 1:
         print("Did you have a nightmare?\n1. Yes\n2. No\n")
         s2 = int(input())
         # Calculate the sleep condition score
-        sql = "SELECT * FROM Book WHERE bookname = %s"
-        print("Search for (book's name) :")
-        searchBook = str(input())
+        score = 0
+        if s1 == 1:
+            score += 5
+        elif s1 == 2:
+            score += 3
+        elif s1 == 3:
+            score += 1
+        if s2 == 1:
+            score += 1
+        elif s2 == 2:
+            score += 5
+        
+        if score > 7:
+            condition = "Good"
+        elif score > 4:
+            condition = "So so"
+        else:
+            condition = "Bad"
+        print("Condition : ", condition)
         cursor.execute(sql, searchBook)
         result = cursor.fetchall()
         for data in result:
             print(data)
     elif menu == 4:
         print("[------------View Montly Report-------------]")
+        print("Select the week")
         sql = "SELECT * FROM Book"
         cursor.execute(sql)
         result = cursor.fetchall()
@@ -73,6 +98,11 @@ while 1:
         print("[------------View Montly Report-------------]")
     elif menu ==6:
         print("[-----------View all sleep data-------------]")
+        sql = "SELECT * FROM SLEEP_TIME"
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        for row_data in result:
+            print(row_data)
     elif menu == 7:
         break
 
